e69240c28a152c5a5cc56bd28a25e821010442bc
Finn Thye <piqueen@gmail.com>
2021-02-26 11:13:07 -0700

    Set up user input exercise with km conversion

---
+++ python-input-km-exercise/index.py
@@ -0,0 +1,7 @@
+# - Create a distance converter converting Km to miles
+# - Take two inputs from user: Their first name and the distance in km
+# - Print: Greet user by name and show km, and mile values
+# - 1 mile is 1.609 kilometers
+# - hint: use correct types for calculating and print
+# - Did you capitalize the name
+
