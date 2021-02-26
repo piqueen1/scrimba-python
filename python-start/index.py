220f3694b96947af786a884d3f5915bb307c1911
Finn Thye <piqueen@gmail.com>
2021-02-26 10:37:28 -0700

    Scrimba solution to slicing exercise

--- python-start/index.py
+++ python-start/index.py
@@ -1,5 +1,5 @@
 msg = 'Welcome to Python 101: Strings'
-msg2 = msg[20] + ' ' + msg[:7] + ' ' + msg[25:29].title() + ' ' + msg[8:10].title() + ' ' + 'Tyler'
-print(msg2)
-print(msg2[::-1])
+msg2 = msg[20] + ' ' + msg[:7] + ' ' + msg[25:29]+msg[7:11]+'Tyler'
+print(msg2.title())
+print(msg2[::-1].title())
 # 1 Welcome Ring To Tyler
