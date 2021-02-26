6514e47ba4acf03f0a91fe0000278d5368c007cf
Finn Thye <piqueen@gmail.com>
2021-02-26 10:59:13 -0700

    String interpolation, capitalize() and lower()

--- python-start/index.py
+++ python-start/index.py
@@ -1,5 +1,10 @@
 msg = 'Welcome to Python 101: Strings'
-msg2 = msg[20] + ' ' + msg[:7] + ' ' + msg[25:29]+msg[7:11]+'Tyler'
-print(msg2.title())
-print(msg2[::-1].title())
+# msg2 = msg[20] + ' ' + msg[:7] + ' ' + msg[25:29]+msg[7:11]+'Tyler'
+# print(msg2.title())
+# print(msg2[::-1].title())
 # 1 Welcome Ring To Tyler
+
+name='FINN'
+color='PURPLE'
+msg1=f'{name.capitalize()} loves the color {color.lower()}!'
+print(msg1)
