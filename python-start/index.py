bcf697c3338b081fcdf4fff1659c24b550b80f81
Finn Thye <piqueen@gmail.com>
2021-02-26 10:30:39 -0700

    My solution to slice string challenge

--- python-start/index.py
+++ python-start/index.py
@@ -1,2 +1,5 @@
 msg = 'Welcome to Python 101: Strings'
-print(msg[20],msg[:7],msg[27:])
+msg2 = msg[20] + ' ' + msg[:7] + ' ' + msg[25:29].title() + ' ' + msg[8:10].title() + ' ' + 'Tyler'
+print(msg2)
+print(msg2[::-1])
+# 1 Welcome Ring To Tyler
