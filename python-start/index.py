cec7727d674abbbd373ed8b1bac74b685c923cdb
Finn Thye <piqueen@gmail.com>
2021-02-26 11:09:27 -0700

    Use inputs and typecasting for text and float calculation

--- python-start/index.py
+++ python-start/index.py
@@ -1,10 +1,23 @@
-msg = 'Welcome to Python 101: Strings'
+#Slicing and reverse with [::-1]
+# msg = 'Welcome to Python 101: Strings'
 # msg2 = msg[20] + ' ' + msg[:7] + ' ' + msg[25:29]+msg[7:11]+'Tyler'
 # print(msg2.title())
 # print(msg2[::-1].title())
 # 1 Welcome Ring To Tyler
 
-name='FINN'
-color='PURPLE'
-msg1=f'{name.capitalize()} loves the color {color.lower()}!'
-print(msg1)
+#String Interpolation
+# name='FINN'
+# color='PURPLE'
+# msg1=f'{name.capitalize()} loves the color {color.lower()}!'
+# print(msg1)
+
+# Inputs
+# name= input('What is your name?')
+# age=input('What is your age?')
+# print(f'Hello {name}.  You are {age} years old.')
+
+#Calculator
+num1= input('Enter a digit')
+num2= input('Enter another digit')
+answer=float(num1)+float(num2)
+print(answer)
