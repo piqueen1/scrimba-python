1b4f64aae1f0e14dd0da0824669cbca54b5a3c4a
Finn Thye <piqueen@gmail.com>
2021-02-27 08:41:42 -0700

    Solution to name-list exercise

--- python-name-list/index.py
+++ python-name-list/index.py
@@ -1,7 +1,6 @@
 
-csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'
-friends_list = ['Exercise: fill me with names']
-print(friends_list)
+csv = 'Eric,John,Michael,Terry,Graham:TerryG;Brian'.replace(';',',').replace(':',',').split(',')
+print(csv)
 # From the list above fill a list(friends_list) properly
 # with the names of all the friends. One per "slot"
 # you may need to run same command several times
