2def1e13f50b3d98fb3f0ec6847f7b11bcb461c7
Finn Thye <piqueen@gmail.com>
2021-02-27 09:33:27 -0700

    Complete set exercise

--- python-set-exercise/index.py
+++ python-set-exercise/index.py
@@ -1,12 +1,28 @@
 #Sets - Exercise
 
-#1. Check if ‘Eric’ and ‘John’ exist in friends
-#2. combine or add the two sets 
-#3. Find names that are in both sets
-#4. find names that are only in friends
-#5. Show only the names who only appear in one of the lists
-#6. Create a new cars-list without duplicates
 
 friends = {'John','Michael','Terry','Eric','Graham'}
 my_friends = {'Reg','Loretta','Colin','John','Graham'}
 cars =['900','420','V70','911','996','V90','911','911','S','328','900']
+
+#1. Check if ‘Eric’ and ‘John’ exist in friends
+print('Eric' in friends and 'John' in friends)
+#2. combine or add the two sets 
+combined = friends.union(my_friends)
+print(combined)
+combinedPipe = friends | my_friends
+print(combinedPipe)
+#3. Find names that are in both sets
+print (friends.intersection(my_friends))
+intersection2 = friends & my_friends
+#4. find names that are only in friends
+print(friends.difference(my_friends))
+difference2 = friends - my_friends
+print(difference2)
+#5. Show only the names who only appear in one of the lists
+print(my_friends.symmetric_difference(friends))
+symmetric2 = my_friends ^ friends
+#6. Create a new cars-list without duplicates
+print(set(cars))
+#7. Make a list of unique cars
+unique_cars_list = list(set(cars))
