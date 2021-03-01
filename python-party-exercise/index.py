6379f1779e1eab67e9060a2c27d1c5bf349367f0
Finn Thye <piqueen@gmail.com>
2021-03-01 09:07:38 -0700

    Solve invitation exercise

--- python-party-exercise/index.py
+++ python-party-exercise/index.py
@@ -1,3 +1,11 @@
 names = ['john ClEEse','Eric IDLE','michael']
 names1 = ['graHam chapman', 'TERRY', 'terry jones']
+names.extend(names1)
 
+new_name_1 = input('Please add another friend to invite')
+names.append(new_name_1)
+new_name_2 = input('Please add another friend to invite')
+names.append(new_name_2)
+
+for name in names:
+  print(f'Hello {name.title()}! You are invited to the party Saturday!')
