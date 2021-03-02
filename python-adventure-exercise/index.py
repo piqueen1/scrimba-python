2d7bde735bca58cd9b6a3576c03cb976808e64e7
Finn Thye <piqueen@gmail.com>
2021-03-02 08:57:46 -0700

    Adventure part 1 complete

--- python-adventure-exercise/index.py
+++ python-adventure-exercise/index.py
@@ -13,14 +13,14 @@
 freelancers = {'name':'freelancing Shop','brian': 70, 'black knight':20, 'biccus diccus':100, 'grim reaper':500, 'minstrel':-15}
 antiques = {'name':'Antique Shop','french castle':400, 'wooden grail':3, 'scythe':150, 'catapult':75, 'german joke':5}
 pet_shop = {'name':'Pet Shop','blue parrot':10, 'white rabbit':5, 'newt': 2}
-stores = [freelancers, antiques, pet_shop]
 #create an dempty shopping cart
 cart = {}
 #loop through stores/dicts
-for store in stores :
+for shop in (freelancers,antiques,pet_shop) :
      #inputbox  to show what you can buy...capture textstring of what was bought...make lowercase
-    buy_item = input(f'Welcome to {store['name']}! what do you want to buy: {store[1:]}')
+    buy_item = input(f'Welcome to {shop["name"]}! what do you want to buy: {shop}').lower()
     #update the cart
-    cart.update({buy_item : store[buy_item]}) 
+    cart.update({buy_item : shop.pop(buy_item)}) 
     # use pop...
-print(f'You Purchased {cart.keys} Today it is all free. Have a nice day of mayhem!')
+    bought_items = " and a ".join(list(cart.keys()))
+print(f'You purchased a {bought_items}. Today it is all free. Have a nice day of mayhem!')
